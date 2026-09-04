cb399e1ba3cfe5f2a9fbc41706e39a3333d9cb7d
lungbawmr-beep <lungbawmr@gmail.com>
2026-09-05 04:59:57 +0800

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FRIDAY-v1.1"
+
+include(":android:app")
